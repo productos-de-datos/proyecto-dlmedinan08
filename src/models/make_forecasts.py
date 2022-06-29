d0825bd2eefd8f829c249d96b00ba0d37c7de876
Diana Lucía Medina Nieto <95106092+dlmedinan08@users.noreply.github.com>
2022-06-28 21:30:08 -0500

    Progress_05

--- src/models/make_forecasts.py
+++ src/models/make_forecasts.py
@@ -12,6 +12,33 @@ def make_forecasts():
 
 
     """
+    import pandas as pd
+    import numpy as np
+    from sklearn import linear_model
+    from sklearn.metrics import r2_score
+    import pickle
+
+    precios_diarios = pd.read_csv('data_lake/business/features/precios-diarios.csv', index_col=None)
+    precios_diarios['Fecha'] = pd.to_datetime(precios_diarios['Fecha'], format= '%Y-%m-%d')
+    precios_diarios['year'] = precios_diarios['Fecha'].dt.year
+    precios_diarios['month'] = precios_diarios['Fecha'].dt.month
+    precios_diarios['day'] = precios_diarios['Fecha'].dt.day
+
+    precios_diarios.dropna(inplace=True)
+
+    X = precios_diarios.copy().drop(columns = 'Fecha')
+    y = X.pop('precio')
+
+    regression = pickle.load(open('src/models/precios-diarios.pkl', 'rb'))
+    prediction = regression.predict(X)
+
+    r2_score(y,regression.predict(X))
+
+    precios_diarios['Prediction'] = prediction
+
+    precios_diarios[['Fecha', 'precio', 'Prediction']].to_csv('data_lake/business/forecasts/precios-diarios.csv', index=None)
+
+    return
     raise NotImplementedError("Implementar esta función")
 
 
@@ -19,3 +46,4 @@ if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
+    make_forecasts()
--- src/models/train_daily_model.py
+++ src/models/train_daily_model.py
@@ -6,6 +6,36 @@ def train_daily_model():
 
 
     """
+    import pandas as pd
+    import numpy as np
+    from sklearn import linear_model
+    from sklearn.metrics import r2_score
+    import pickle
+
+    precios_diarios = pd.read_csv('data_lake/business/features/precios-diarios.csv', index_col=None)
+    precios_diarios['Fecha'] = pd.to_datetime(precios_diarios['Fecha'], format= '%Y-%m-%d')
+    precios_diarios['year'] = precios_diarios['Fecha'].dt.year
+    precios_diarios['month'] = precios_diarios['Fecha'].dt.month
+    precios_diarios['day'] = precios_diarios['Fecha'].dt.day
+
+    precios_diarios.dropna(inplace=True)
+
+    X = precios_diarios.copy().drop(columns = 'Fecha')
+    y = X.pop('precio')
+    
+    X_train = X[:round(X.shape[0]*0.75)]
+    X_test = X[round(X.shape[0]*0.75):]
+    y_train = y[:round(y.shape[0]*0.75)]
+    y_test = y[round(y.shape[0]*0.75):]
+    
+    regression = linear_model.LinearRegression()
+    regression.fit(X_train, y_train)
+
+    r2_score(y_test,regression.predict(X_test))
+
+    pickle.dump(regression, open('src/models/precios-diarios.pkl', 'wb'))
+
+    return
     raise NotImplementedError("Implementar esta función")
 
 
@@ -13,3 +43,4 @@ if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
+    train_daily_model()
